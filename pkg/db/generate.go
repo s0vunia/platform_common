245e5eda307f5f0ea3af07ab1309b28dc03d6b92
s0vunia <burindima254@yandex.ru>
2025-02-12 18:45:55 +0300

    add mocks DB

--- pkg/db/generate.go
+++ pkg/db/generate.go
@@ -3,3 +3,4 @@ package db
 //go:generate sh -c "rm -rf mocks && mkdir -p mocks"
 //go:generate minimock -i TxManager -o ./mocks/ -s "_minimock.go"
 //go:generate minimock -i Client -o ./mocks/ -s "_minimock.go"
+//go:generate minimock -i DB -o ./mocks/ -s "_minimock.go"
